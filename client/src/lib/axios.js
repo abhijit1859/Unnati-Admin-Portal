348b3be303155f7ef36c3b196e0e3e1525b08a75
abhijit1859 <kumarabhijit936@gmail.com>
2025-10-09 11:21:19 +0530

    minor fixes

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL ,
+    baseURL: "https://unnati-admin-portal-1.onrender.com/api/v1" ,
     // || "http://localhost:8000/api/v1",
     withCredentials: true,
 });
